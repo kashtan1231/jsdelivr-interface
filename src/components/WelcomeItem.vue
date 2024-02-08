e6eb04a6d94d34ffa92b3aa0a86df3aba35fb172
kostya <6287026@stud.nau.edu.ua>
2024-02-08 11:48:58 +0200

    test

--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -13,74 +13,74 @@
 </template>
 
 <style scoped>
-.item {
-  position: relative;
-  display: flex;
-  margin-top: 2rem;
-}
-
-.details {
-  flex: 1;
-  margin-left: 1rem;
-}
-
-i {
-  display: flex;
-  place-content: center;
-  place-items: center;
-  width: 32px;
-  height: 32px;
-  color: var(--color-text);
-}
-
-h3 {
-  margin-bottom: 0.4rem;
-  color: var(--color-heading);
-  font-size: 1.2rem;
-  font-weight: 500;
-}
-
-@media (min-width: 1024px) {
   .item {
-    margin-top: 0;
-    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+    position: relative;
+    display: flex;
+    margin-top: 2rem;
+  }
+
+  .details {
+    flex: 1;
+    margin-left: 1rem;
   }
 
   i {
-    position: absolute;
-    top: calc(50% - 25px);
-    left: -26px;
-    width: 50px;
-    height: 50px;
-    border: 1px solid var(--color-border);
-    border-radius: 8px;
-    background: var(--color-background);
+    display: flex;
+    place-content: center;
+    place-items: center;
+    width: 32px;
+    height: 32px;
+    color: var(--color-text);
   }
 
-  .item::before {
-    content: ' ';
-    position: absolute;
-    bottom: calc(50% + 25px);
-    left: 0;
-    height: calc(50% - 25px);
-    border-left: 1px solid var(--color-border);
+  h3 {
+    margin-bottom: 0.4rem;
+    color: var(--color-heading);
+    font-size: 1.2rem;
+    font-weight: 500;
   }
 
-  .item::after {
-    content: ' ';
-    position: absolute;
-    top: calc(50% + 25px);
-    left: 0;
-    height: calc(50% - 25px);
-    border-left: 1px solid var(--color-border);
-  }
+  @media (min-width: 1024px) {
+    .item {
+      margin-top: 0;
+      padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+    }
 
-  .item:first-of-type::before {
-    display: none;
-  }
+    i {
+      position: absolute;
+      top: calc(50% - 25px);
+      left: -26px;
+      width: 50px;
+      height: 50px;
+      border: 1px solid var(--color-border);
+      border-radius: 8px;
+      background: var(--color-background);
+    }
 
-  .item:last-of-type::after {
-    display: none;
+    .item::before {
+      content: ' ';
+      position: absolute;
+      bottom: calc(50% + 25px);
+      left: 0;
+      height: calc(50% - 25px);
+      border-left: 1px solid var(--color-border);
+    }
+
+    .item::after {
+      content: ' ';
+      position: absolute;
+      top: calc(50% + 25px);
+      left: 0;
+      height: calc(50% - 25px);
+      border-left: 1px solid var(--color-border);
+    }
+
+    .item:first-of-type::before {
+      display: none;
+    }
+
+    .item:last-of-type::after {
+      display: none;
+    }
   }
-}
 </style>
