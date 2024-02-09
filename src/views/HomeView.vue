c96f6811e7bd183a31118f53a7d583b7c775d52b
Kravckenko Konstantin <67584196+kashtan1231@users.noreply.github.com>
2024-02-09 17:35:22 +0200

    Pagination (#4)
    * done
    
    * lint

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,20 +4,55 @@
 
     <p class="home-view__description">Type your package here</p>
 
-    <BaseInput v-model.trim="search" class="home-view__search" placeholder="Type here" />
+    <BaseInput
+      v-model.trim="search"
+      class="home-view__search"
+      placeholder="Type here"
+      @update:model-value="onInput"
+    />
 
-    <TablePackages v-if="search && searchedPackages.length > 0" :list="searchedPackages" />
-    <h5 v-else-if="searchedPackages.length > 0" class="home-view__nothing">Here will be results</h5>
-    <h5 v-else class="home-view__nothing">Not found packages</h5>
+    <template v-if="search && packageStore.PAGE_PACKAGES.length > 0">
+      <TablePackages :list="packageStore.PAGE_PACKAGES" />
+
+      <div v-if="packageStore.PAGE_COUNT > 1" class="home-view__buttons">
+        <BaseButton
+          class="home-view__buttons-prev"
+          :disabled="packageStore.PAGE <= 1"
+          @click="prevPage"
+          >Prev</BaseButton
+        >
+        <BaseButton
+          v-for="item in packageStore.PAGE_COUNT"
+          :key="item"
+          :class="[
+            'home-view__buttons-pages',
+            { 'home-view__buttons-pages--active': packageStore.PAGE === item },
+          ]"
+          @click="packageStore.SET_PAGE(item)"
+        />
+        <BaseButton
+          :disabled="packageStore.PAGE_COUNT <= packageStore.PAGE"
+          class="home-view__buttons-next"
+          @click="nextPage"
+          >Next</BaseButton
+        >
+      </div>
+    </template>
+    <h5 v-else-if="search" class="home-view__nothing">Packages not found</h5>
+    <h5 v-else class="home-view__nothing">Here will be results</h5>
   </div>
 </template>
 
 <script async setup lang="ts">
-  import { ref, computed } from 'vue'
+  import { onUnmounted, ref } from 'vue'
   import { usePackageStore } from '@/stores/package'
   import BaseInput from '@/components/BaseInput.vue'
   import TablePackages from '@/components/TablePackages.vue'
   import type { IPackage } from '@/types/package'
+  import { useRouter } from 'vue-router'
+  import BaseButton from '@/components/BaseButton.vue'
+
+  const router = useRouter()
 
   const search = ref('')
 
@@ -33,8 +68,28 @@
     packageStore.SET_PACKAGES(sessionStorePackages)
   }
 
-  const searchedPackages = computed(() => {
-    return packageStore.PACKAGES.filter((item: IPackage) => item.name.includes(search.value))
+  const prevPage = () => {
+    packageStore.PREV_PAGE()
+    router.push({ name: 'HomeViewPage', params: { page: packageStore.PAGE } })
+  }
+
+  const nextPage = () => {
+    packageStore.NEXT_PAGE()
+    router.push({ name: 'HomeViewPage', params: { page: packageStore.PAGE } })
+  }
+
+  const onInput = () => {
+    if (timeout.value) clearTimeout(timeout.value)
+
+    timeout.value = setTimeout(() => {
+      packageStore.SET_SEARCHED_PACKAGES(search.value)
+    }, 200)
+  }
+
+  const timeout = ref<undefined | number>(undefined)
+
+  onUnmounted(() => {
+    clearTimeout(timeout.value)
   })
 </script>
 
@@ -43,6 +98,7 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+    height: 100%;
 
     &__title {
       margin-bottom: 32px;
@@ -60,6 +116,29 @@
 
     &__nothing {
       color: $gray;
+    }
+
+    &__buttons {
+      display: flex;
+      gap: 8px;
+      align-items: center;
+      margin-top: 32px;
+
+      &-pages {
+        width: 12px;
+        height: 12px;
+        border-radius: 50%;
+        background-color: $gray-light;
+        transition-duration: 0.2s;
+
+        &:hover {
+          background-color: $gray;
+        }
+
+        &--active {
+          background-color: $gray;
+        }
+      }
     }
   }
 </style>
